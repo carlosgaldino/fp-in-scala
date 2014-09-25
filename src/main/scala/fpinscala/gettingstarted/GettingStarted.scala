c1f4865c920c8ec752f2ef4ec4cc8916ab19d35f
Carlos Galdino <carloshsgaldino@gmail.com>
2014-09-25 14:10:15 -0300

    Add code up to section 2.4.

--- src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -5,12 +5,31 @@ object MyModule {
     if (x < 0) -x
     else x
 
-  def formatAbs(x: Int): String = {
-    val msg = "The absolute value of %d is %d"
-    msg.format(x, abs(x))
+  def factorial(x: Int): Int = {
+    @annotation.tailrec
+    def go(x: Int, acc: Int): Int =
+      if (x <= 0) acc
+      else go(x - 1, acc * x)
+
+    go(x, 1)
+  }
+
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(n: Int, a: Int, b: Int): Int =
+      if (n == 0) a
+      else go(n - 1, b, a + b)
+
+    go(n, 0, 1)
+  }
+
+  private def formatResult(name: String, x: Int, f: Int => Int): String = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, x, f(x))
   }
 
   def main(args: Array[String]): Unit = {
-    println(formatAbs(56))
+    println(formatResult("absolute value", 56, abs))
+    println(formatResult("factorial", 7, factorial))
   }
 }
