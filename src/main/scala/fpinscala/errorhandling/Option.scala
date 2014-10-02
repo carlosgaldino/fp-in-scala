37b6796058e14a79a3cad1aeea256036a164e99c
Carlos Galdino <carloshsgaldino@gmail.com>
2014-10-02 08:45:21 -0300

    Add initial exercises from Chapter 4.

---
+++ src/main/scala/fpinscala/errorhandling/Option.scala
@@ -0,0 +1,43 @@
+package fpinscala.errorhandling
+
+import scala.{Option => _, _} /* Hide default implementation */
+
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(v) => Some(f(v))
+  }
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(v) => v
+  }
+
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f) getOrElse None
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] =
+    this map (Some(_)) getOrElse ob
+
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(a => if (f(a)) Some(a) else None)
+}
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+object Option {
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+    mean(xs) flatMap (m => mean(xs.map(x => math.pow(x - m, 2))))
+
+  def map2[A, B, C](a: Option[A], b: Option[B])(f: (A ,B) => C): Option[C] =
+    a flatMap (aa => b map (bb => f(aa, bb)))
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case h :: t => h flatMap (hh => sequence(t) map (hh :: _))
+  }
+}
